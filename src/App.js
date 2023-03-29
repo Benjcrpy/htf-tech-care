2a581d9bf38516a0e9fca9797f7a91079cf09689
BENJ <Anthonyagapay709@gmail.com>
2023-03-29 08:39:08 +0800

    Added jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Service from "./components/Service";
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Hero/>
+      <Service/>
     </div>
   );
 }
