468b0fa332b5d379f4d0dccbc58a6374c36b684c
BENJ <Anthonyagapay709@gmail.com>
2023-04-03 05:57:33 +0800

    update card jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = () => {
        <div className='w-full bg-slate-900 shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-500'>
          <img className='w-20  mx-auto mt-[-3rem]' src={hard1} alt=""/>
          <h2 className='text-2xl font-bold text-center py-8'>HARDWARE CARE</h2>
-         <p className='text-center text-4xl font-bold'>1M</p>
+         <p className='text-center text-4xl font-bold'></p>
          <div className='text-center font-medium'>
             <p className='py-2 border-b mx-8 mt-8'>Cleaning</p>
             <p className='py-2 border-b mx-8'>Repair</p>
@@ -21,7 +21,7 @@ const Card = () => {
         <div className='w-full bg-slate-900 shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-500'>
          <img className='w-20  mx-auto mt-[-3rem]' src={hard1} alt=""/>
          <h2 className='text-2xl font-bold text-center py-8'>SOFTWARE CARE</h2>
-         <p className='text-center text-4xl font-bold'>1M</p>
+         <p className='text-center text-4xl font-bold'></p>
          <div className='text-center font-medium'>
             <p className='py-2 border-b mx-8 mt-8'>Built Website</p>
             <p className='py-2 border-b mx-8'>Commission</p>
@@ -34,12 +34,12 @@ const Card = () => {
         </div>
         <div className='w-full bg-slate-900 shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-500'>
          <img className='w-20  mx-auto mt-[-3rem]' src={hard1} alt=""/>
-         <h2 className='text-2xl font-bold text-center py-8'>HARDWARE CARE</h2>
-         <p className='text-center text-4xl font-bold'>1M</p>
+         <h2 className='text-2xl font-bold text-center py-8'>HardWare Component</h2>
+         <p className='text-center text-4xl font-bold'></p>
          <div className='text-center font-medium'>
-            <p className='py-2 border-b mx-8 mt-8'>Cleaning</p>
-            <p className='py-2 border-b mx-8'>Repair</p>
-            <p className='py-2 border-b mx-8'>Built Computer</p>
+            <p className='py-2 border-b mx-8 mt-8'>GPU </p>
+            <p className='py-2 border-b mx-8'>Cpu</p>
+            <p className='py-2 border-b mx-8'>RAm</p>
          </div>
          <button className='bg-lime-400 hover:bg-lime-700 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:text-slate-300'>Try Now!</button>
         </div>
